010abf9a9484b6947034bc40062e997e043f5fcf
Les Matheson <les.matheson@gmail.com>
2024-12-20 06:10:01 -0500

    Upstream: use hard-coded output port

--- cc11_fix/filter.py
+++ cc11_fix/filter.py
@@ -6,9 +6,8 @@ def list_ports():
         print(f"  {name}")
 
 def filter_midi(input_port_name):
-    output_port_name = 'midi-filtered-cc11'
     try:
-        with mido.open_output(output_port_name) as midiout:
+        with mido.open_output('midi-filtered-cc11 5') as midiout:
             with mido.open_input(input_port_name) as inport:
                 for msg in inport:
                     if not (msg.type == 'control_change' and msg.control == 11):
